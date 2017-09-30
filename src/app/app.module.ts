ca5bd2f3362e3b795f0fd285999e9ef55d36dca6
Alan Taylor <alancarltaylor@gmail.com>
2017-09-30 15:31:18 -0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule } from '@angular/material';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+
+
+const ROUTES = [
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      { path: 'confirmation', component: ConfirmationComponent  }
+    ]
+  }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConfirmationComponent,
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES, { useHash: true }),
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdMenuModule,
+    MdCardModule,
+    MdToolbarModule,
+    MdIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
